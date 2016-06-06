df6e744855d7081066cf97daedd7c58570fca038
Koen Dirk van Esterik <koen@flipflopinteractive.com>
2016-06-06 12:02:15 +0200

    Finishing up styling for laptop

--- content/themes/drive/gulpfile.js
+++ content/themes/drive/gulpfile.js
@@ -1,71 +1,69 @@
-var gulp        = require('gulp'),
-    concat      = require('gulp-concat'),
-    exec        = require('gulp-exec'),
-    watch       = require('gulp-watch'),
-    livereload  = require('gulp-livereload'),
-    lr          = require('tiny-lr'),
-    server      = lr();
+var gulp         = require( 'gulp' ),
+	less         = require( 'gulp-less' ),
+	autoprefixer = require( 'gulp-autoprefixer' ),
+	concat       = require( 'gulp-concat' ),
+	exec         = require( 'gulp-exec' ),
+	watch        = require( 'gulp-watch' ),
+	livereload   = require( 'gulp-livereload' ),
+	lr           = require( 'tiny-lr' ),
+	server       = lr();
 
 
 gulp.task( 'template', function () {
-  return gulp.src('*.hbs')
-    .pipe( livereload( server ));
+	return gulp.src( '*.hbs' )
+		.pipe( livereload( server ));
 });
 
 
-gulp.task( 'style', function () {
-  return gulp.src([
-      'bower_components/normalize-css/normalize.css',
-      'dev/styles/boilerplate.css',
-      'dev/styles/main.css'
-    ])
-    .pipe( concat( 'main.css' ))
-    .pipe( gulp.dest( 'assets/css' ))
-    .pipe( livereload( server ));
-});
-
-
-gulp.task( 'scripts', function () {
-  return gulp.src([
-      'dev/scripts/main.js'
-    ])
-    .pipe( concat( 'main.js' ))
-    .pipe( gulp.dest( 'assets/js' ))
-    .pipe( livereload( server ));
+gulp.task( 'style', function(){
+	return gulp.src( './dev/styles/styles.less' )
+		.pipe( less() )
+		.pipe( autoprefixer({ 'browsers': [ '> 5%' ]}))
+		.pipe( gulp.dest( 'assets/css' ))
+		.pipe( livereload( server ));
 });
 
 
 gulp.task( 'lib', function () {
-  return gulp.src([
-      'bower_components/modernizr/modernizr.js',
-      'dev/scripts/livereload.js'
-    ])
-    .pipe( concat( 'lib.js' ))
-    .pipe( gulp.dest( 'assets/js' ))
-    .pipe( livereload( server ));
+	return gulp.src([
+			'bower_components/modernizr/modernizr.js'
+		])
+		.pipe( concat( 'lib.js' ))
+		.pipe( gulp.dest( 'assets/js' ))
+		.pipe( livereload( server ));
 });
 
 
-gulp.task('ghost', function() {
-  gulp.src('')
-    .pipe(exec( 'cd ../../../; npm start' ));
+gulp.task( 'scripts', function () {
+	return gulp.src([
+			'dev/scripts/main.js'
+		])
+		.pipe( concat( 'main.js' ))
+		.pipe( gulp.dest( 'assets/js' ))
+		.pipe( livereload( server ));
 });
 
 
-gulp.task('watch', function() {
-  // Listen on port 35729
-  server.listen( 35729, function ( err ) {
-    if ( err ) {
-     return console.log( err );
-    };
-
-    gulp.watch( 'dev/styles/*.css', ['style'] );
-    gulp.watch( 'dev/scripts/*.js', ['scripts'] );
-    gulp.watch( '*.hbs', ['template'] );
-  });
+gulp.task( 'ghost', function(){
+	gulp.src( '' )
+		.pipe( exec( 'cd ../../../; npm start' ));
 });
 
 
-gulp.task( 'default', ['style', 'lib', 'scripts'], function() {
-    gulp.start( 'ghost', 'watch' );
+gulp.task( 'watch', function(){
+	// Listen on port 35729
+	server.listen( 35729, function( err ){
+		if( err ){
+			return console.log( err );
+		};
+
+		gulp.watch( 'dev/styles/**/*.less', [ 'style' ]);
+		gulp.watch( 'dev/scripts/*.js', [ 'scripts' ]);
+		gulp.watch( '*.hbs', [ 'template' ]);
+	});
+});
+
+
+gulp.task( 'default', [ 'style', 'lib', 'scripts' ], function(){
+		gulp.start( 'ghost', 'watch' );
 });
