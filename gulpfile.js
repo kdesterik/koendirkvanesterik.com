4e8e0b6e60161eba1f7cd54c1844db69092ce1c7
Koen Dirk van Esterik <koen@flipflopinteractive.com>
2016-06-07 22:41:58 +0200

    Adding original image setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp         = require( 'gulp' ),
 	less         = require( 'gulp-less' ),
 	autoprefixer = require( 'gulp-autoprefixer' ),
+	browserify 	 = require( 'browserify' ),
+	tap 		 = require( 'gulp-tap' ),
+	jshint 		 = require( 'gulp-jshint' ),
+	uglify 		 = require( 'gulp-uglify' );
 	concat       = require( 'gulp-concat' ),
 	exec         = require( 'gulp-exec' ),
 	watch        = require( 'gulp-watch' ),
 	livereload   = require( 'gulp-livereload' ),
 	lr           = require( 'tiny-lr' ),
-	server       = lr();
+	server       = lr(),
+	fs 			 = require( 'fs' ),
+	SSH 		 = require( 'gulp-ssh' );
 
 
 gulp.task( 'template', function () {
@@ -24,23 +30,25 @@ gulp.task( 'style', function(){
 });
 
 
-gulp.task( 'lib', function () {
+gulp.task( 'libs', function () {
 	return gulp.src([
-			'bower_components/modernizr/modernizr.js'
+			'./bower_components/modernizr/modernizr.js'
 		])
-		.pipe( concat( 'lib.js' ))
-		.pipe( gulp.dest( 'assets/js' ))
+		.pipe( concat( 'libs.js' ))
+		.pipe( gulp.dest( './assets/js' ))
 		.pipe( livereload( server ));
 });
 
 
-gulp.task( 'scripts', function () {
-	return gulp.src([
-			'dev/scripts/main.js'
-		])
-		.pipe( concat( 'main.js' ))
-		.pipe( gulp.dest( 'assets/js' ))
-		.pipe( livereload( server ));
+gulp.task( 'scripts', function(){
+	return gulp.src( './dev/scripts/*.js' )
+		.pipe( jshint() )
+  		.pipe( jshint.reporter( 'jshint-stylish' ))
+		.pipe( tap( function( file ){
+		  file.contents = browserify( file.path, { debug: false }).bundle();
+		}))
+		// .pipe( uglify() )
+		.pipe( gulp.dest( './assets/js' ));
 });
 
 
@@ -57,13 +65,46 @@ gulp.task( 'watch', function(){
 			return console.log( err );
 		};
 
-		gulp.watch( 'dev/styles/**/*.less', [ 'style' ]);
-		gulp.watch( 'dev/scripts/*.js', [ 'scripts' ]);
-		gulp.watch( '*.hbs', [ 'template' ]);
+		gulp.watch( './dev/styles/**/*.less', [ 'style' ]);
+		gulp.watch( './dev/scripts/*.js', [ 'scripts' ]);
+		gulp.watch( './*.hbs', [ 'template' ]);
 	});
 });
 
 
-gulp.task( 'default', [ 'style', 'lib', 'scripts' ], function(){
-		gulp.start( 'ghost', 'watch' );
+gulp.task( 'default', [ 'style', 'libs', 'scripts' ], function(){
+	gulp.start( 'ghost', 'watch' );
+});
+
+
+
+var ssh = new SSH({
+
+	'ignoreErrors': false,
+	'sshConfig': {
+
+		'host': '146.185.166.164',
+		'port': 22,
+		'username': 'root',
+		'privateKey': fs.readFileSync( '/Users/kdesterik/.ssh/id_rsa' )
+	}
+})
+
+
+gulp.task( 'deploy', function(){
+	return gulp.src([
+		'./**/*.*',
+		'!**/bower_components/**',
+		'!**/dev/**',
+		'!**/node_modules/**',
+		'!./.editorconfig',
+		'!./.gitignore',
+		'!./.jshintrc',
+		'!./bower.json',
+		'!./gulpfile.js',
+		'!./LICENSE',
+		'!./README.md',
+	])
+    .pipe( ssh.dest( '/var/www/ghost/content/themes/drive/' ))
+    .pipe( ssh.shell([ 'chown -R ghost:ghost /var/www/ghost/content/themes/drive/' ]));
 });
